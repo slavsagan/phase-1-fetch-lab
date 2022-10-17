c1d954d93eba65d2fcf1423f42c06edb7c0f1ad0
Slav <itiksolution@gmail.com>
2022-10-17 12:05:03 +1100

    Assignment complete

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
+const requestURL = 'https://anapioficeandfire.com/api/books'
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(requestURL) /** DONT FORGET RETURN!!!! */
+    .then((resp) => resp.json())
+    .then(function (json) {
+      renderBooks(json)
+      book5(json)
+      // character1031(json)
+    })
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const main = document.querySelector('main')
+  books.forEach((book) => {
+    const p = document.createElement('p')
+    p.innerText = book.name
+    main.appendChild(p)
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+function book5(books) {
+  const main = document.querySelector('main')
+  let h4 = document.createElement('h4')
+  h4.className = '5thbook'
+  h4.innerHTML = `
+  <h4 style="color:red">The 5th book in the series is </h4>
+  `
+  main.appendChild(h4)
+
+  const p = document.createElement('p')
+  p.innerText = books[4].name
+  p.style.color = 'red'
+  document.querySelector('main').appendChild(p)
+}
+
+// function character1031(character) {
+//     const main = document.querySelector('main')
+//     let h4 = document.createElement('h4')
+//     h4.className = '5thbook'
+//     h4.innerHTML = `
+//     <h4 style="color:red">The 1031st character in the series is </h4>
+//     `
+//     main.appendChild(h4)
+
+//   for (unit of character) {
+//     let found = unit.characters[1030]
+//     const p = document.createElement('p')
+//     p.textContent = found
+//     p.style.color = 'red'
+//     document.querySelector('main').appendChild(p)
+//   }  
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks()
+})
